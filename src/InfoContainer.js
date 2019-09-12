b1697ab1e6aa084c84df51996eee62017c50dfb2
hiaximize <dbarkerit@gmail.com>
2019-09-12 15:19:15 -0400

    pushing initial files to github

--- src/InfoContainer.js
+++ src/InfoContainer.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import Member from './Member.js'
 
 class InfoContainer extends React.Component{
     render(){
         return(
             <div id="infoContainer">
-            
+               
+                <table>
+                    <theader className="tableHeader">
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Membership Type</th>
+                        <th>Due Date</th>
+                    </theader>
+                    <tbody>
+                        
+                            <Member />
+                        
+                    </tbody>
+                </table>
+                
             </div>
         )
     }
---
+++ src/Member.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Member extends React.Component{
+    render(){
+        return(
+            <div id="memberRow">
+                <tr>
+                    <td>1</td>
+                    <td>Derek</td>
+                    <td>.</td>
+                    <td>Test</td>
+                </tr>
+            </div>
+        )
+    }
+}
+
+export default Member
