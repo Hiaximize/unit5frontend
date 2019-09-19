895e0b1d5ac40406525ecc2aec236b4af75db085
hiaximize <dbarkerit@gmail.com>
2019-09-18 21:53:54 -0400

    committing work

--- src/Member.js
+++ src/Member.js
@@ -5,7 +5,8 @@ class Member extends React.Component{
     constructor(){
         super()
         this.state={
-            showModal: false
+            showModal: false,
+
         }
 
         this.handleOpenModal = this.handleOpenModal.bind(this)
@@ -30,12 +31,11 @@ class Member extends React.Component{
         return(
             <div id="memberRow">
 
-                {/* ADD ROW CONTENT FOR MEMBER HERE */}
-
                 <button onClick={()=>{
-                    fetch('http://localhost:3000/api', {
-                        method: "GET"
-                    }).then(data=>data.json())
+                    fetch('http://membershiptrackerbackend.herokuapp.com/members', {
+                        method: "GET",
+                        mode: "cors"
+                    }).then(data=>console.log(data.json()))
                     .then(json => console.log(json))}}> Grab Data</button>
 
                 <button onClick={this.handleOpenModal}>Edit Member</button>
@@ -44,9 +44,73 @@ class Member extends React.Component{
                 contentLabel="View Member Details"
                 ariaHideApp={false}>
                 
-                {/* ADD MODAL CONTENT HERE */}
+                    <div id="UpdateMemberContainer">
+                <div id="formContainer">
+                    <form id="addMemberForm">
+                        <fieldset>
+                            <legend>Member Information</legend>
+                                <input name="addMemberFirstName" type="text" id="addMemberFirstName"  className="addMemberInput" placeholder="First Name" required />
 
-                <button onClick={this.handleCloseModal}>Close</button>
+                                <input name="addMemberLastName" type="text" id="addMemberLastName" className="addMemberInput" placeholder="Last Name" required />
+
+                                <input name="addMemberNumber" type="number" id="addMemberPhoneNumber" className="addMemberInput" placeholder="Phone Number" required />
+
+                                <input name="addMemberAddress" type="text" id="addMemberAddress" className="addMemberInput" placeholder="Address" required /><br />
+
+                                <input name="addMemberCity" type="text" id="addMemberCity" className="addMemberInput" placeholder="City" required />
+
+                                <input name="addMemberState" type="text" id="addMemberState" className="addMemberInput" placeholder="State" required />
+
+                                <input name="addMemberZip" type="number" id="addMemberZip" className="addMemberInput" placeholder="Zip" required />
+
+                                <input name="addMemberEmail" type="email" id="addMemberEmailAddress" placeholder="Email" className="addMemberInput" required />                              
+
+                                <br />
+
+                                <div id="addMemberSecondRow">
+                                    <div id="leftHalfSecondRow">
+                                        <fieldset id="membershipInfo">
+                                            <legend>Membership Information</legend>
+
+                                            <label id="basicLabel" className="addMemberLabel" htmlFor="addMemberCheckBoxBasicMembership">Basic</label>
+                                            <input type="checkbox" id="addMemberCheckBoxBasicMembership" className="addMemberInput addMemberCheckBox" />
+
+                                            <label id="silverLabel" className="addMemberLabel" htmlFor="addMemberCheckBoxSilverMembership">Silver</label>
+                                            <input name="addMemberSilver" type="checkbox" className="addMemberInput addMemberCheckBox" id="addMemberCheckBoxSilverMembership" />
+
+                                            <label id="Premium" className="addMemberLabel" htmlFor="addMemberCheckBoxPremiumMembership">Premium</label>
+                                            <input name="addMemberPremium" type="checkbox" id="addMemberCheckBoxPremiumMembership" className="addMemberInput addMemberCheckBox" /><br />
+
+                                            {/* may cause issues here */}
+                                            <input name="addMemberStartDate" id="addMemberStartDate" className="addMemberInput addMemberDates" placeholder="Start Date" type="date" required />
+
+                                            <input name="addMemberDueDate" id="addMemberDueDate" className="addMemberInput addMemberDates" placeholder="Due Date" type="date" required />
+                                        </fieldset>
+                                
+                                        <div id="bottomRow">
+                                            <textarea rows="10" name="addMemberNotes" id="addMemberNotes" className="addMemberInput" placeholder="Notes" />
+                                        
+                                            <input type="submit" id="addMemberSubmitButton" value="ADD MEMBER" />
+
+                                            <button id="addMemberCancelButton" onClick={
+                                                this.handleCloseModal
+                                            }>Cancel</button>
+                                            
+                                        </div>
+
+                                    </div>
+
+                                    <div id="addMemberProfilePhotoContainer">
+                                        <img src="placeholder.png" id="addMemberPicture" alt="profileImage"/>
+                                        
+                                    </div>
+                                </div>
+                                
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+
                 </ReactModal>
             </div>
         )
